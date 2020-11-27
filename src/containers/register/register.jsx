3e0b90cf7b3c94351adce0c738a254589c4ab3eb
zefeigao <137285459@qq.com>
2020-11-27 21:50:03 +0800

    添加logo组件

--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,9 +1,21 @@
 import React, {Component} from 'react'
+import {
+    NavBar,
+    WingBlank,
+    List,
+    InputItem,
+    WhiteSpace,
+    Radio,
+    Button
+} from 'antd-mobile'
+const ListItem = List.Item
 
 export default class Register extends Component {
     render() {
         return(
-            <div>Register</div>
+            <div>
+
+            </div>
         )
     }
 }
